a44e8550c01d93c9a7f769ecf2070fba8ba78362
morenadam <adam.moren1@gmail.com>
2020-10-06 15:45:17 +0200

    add listener

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
 
-# Press Skift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pynput
+from pynput.keyboard import Key, Listener
+import logging
 
+#log_directory = ""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#logging.basicConfig(filename="log_results.txt", level=logging.DEBUG)
 
+def keypress(key):
+    #logging.info(str(key))
+    print(key);
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with Listener(on_press=keypress) as listener:
+        listener.join()
